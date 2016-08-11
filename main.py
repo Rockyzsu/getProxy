8e1e1f201029eeff4d0aab74df8fd5adc88bf411
Rockyzsu <jinweizsu@gmail.com>
2016-08-11 23:41:48 +0800

    add comment in the source code

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ class getProxy():
         et = etree.HTML(content)
         result_even = et.xpath('//tr[@class=""]')
         result_odd = et.xpath('//tr[@class="odd"]')
+        #因为网页源码中class 分开了奇偶两个class，所以使用lxml最方便的方式就是分开获取。
+        #刚开始我使用一个方式获取，因而出现很多不对称的情况，估计是网站会经常修改源码，怕被其他爬虫的抓到
+        #使用上面的方法可以不管网页怎么改，都可以抓到ip 和port
         for i in result_even:
             t1 = i.xpath("./td/text()")[:2]
             print "IP:%s\tPort:%s" % (t1[0], t1[1])
